56c35d3b3da99b301dcfa8fca3d8164b9bc2ae96
Pedro <phpontesmartins19@gmail.com>
2024-08-15 15:36:19 -0300

    :sparkles: feat: Adição dos novos testes

--- Frontend/cypress/e2e/teste.cy.js
+++ Frontend/cypress/e2e/teste.cy.js
@@ -1,13 +1,37 @@
+//Testa o caminho
 /*describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:3000')
   })
 }) */
 
+//Testa o testo das vantagens
+// describe('Página Principal', () => {
+//   it('Deve renderizar h1 com o texto correto!', () => {
+//     cy.visit('http://localhost:3000')
+//     cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+//     cy.get('[data-test="vantagens"]').contains('Vantagens do nosso banco:')
+//   })
+// })
+
+
+//Testa se possui um h1 com o texto especificado
+// describe('Página Principal', () => {
+//   it('passes', () => {
+//     cy.visit('localhost:3000')
+//     cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+//   })
+// }) 
+
+//Testa se o elemento com o data-test especificado possui o texto correto
 describe('Página Principal', () => {
   it('Deve renderizar h1 com o texto correto!', () => {
     cy.visit('http://localhost:3000')
     cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
-    cy.get('[data-test="vantagens"]').contains('Vantagens do nosso banco:')
   })
 })
+
+
+
+
+
