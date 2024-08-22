5c26b10b3655d8185010a14754508ca8e8684e7b
Pedro <phpontesmartins19@gmail.com>
2024-08-22 10:32:19 -0300

    :test_tube: test: Correção dos testes da atividade 7

---
+++ Frontend/cypress/e2e/formularioLogin.cy.js
@@ -0,0 +1,16 @@
+describe('Testando múltiplas páginas', () => {
+  beforeEach(()=> {
+    cy.visit('http://localhost:3000')
+    // antes de qualquer teste, é necessário visitar o site da aplicação. Por isto, utilizamos o hook beforeEach
+    // PS: NUNCA altere as portas
+    })
+
+    it('Não deve permitir um e-mail inválido!', () => {
+      cy.getByData('botao-login').click()
+      cy.getByData('email-input').type('neilton@alura')
+      cy.getByData('senha-input').type('123456')
+      cy.getByData('botao-enviar').click()
+      cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
+    })
+})
+
