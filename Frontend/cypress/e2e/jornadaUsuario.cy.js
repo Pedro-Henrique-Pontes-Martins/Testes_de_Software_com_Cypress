78522bfd0fd1dfa6285c6d3c590fc390131715f2
Pedro <phpontesmartins19@gmail.com>
2024-08-23 14:11:53 -0300

    :bulb: docs: Alteração dos comentários de jornadaUsuario

--- Frontend/cypress/e2e/jornadaUsuario.cy.js
+++ Frontend/cypress/e2e/jornadaUsuario.cy.js
@@ -26,14 +26,12 @@ describe('Jornadas de usuário', () => {
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
     cy.location('pathname').should('eq','/home')
-    cy.getByData('select-opcoes').select('Depósito') /* no formulário, 'select-opcoes' é o campo onde
+    cy.getByData('select-opcoes').select('Depósito') 
     
-    selecionamos qual tipo de transação. Escolhemos 'Transferência' */
-    
-    cy.getByData('form-input').type('80') /* incluímos um valor para a transferência*/
+    cy.getByData('form-input').type('80')
     cy.getByData('realiza-transacao').click()
     
-    cy.getByData('lista-transacoes').find('h3').last().contains('R$ 80') /* no histórico de transações (ul), encontraremos um li, o último, que contenha o valor que inserimos */
+    cy.getByData('lista-transacoes').find('h3').last().contains('R$ 80') 
     
     cy.getByData('botao-sair').click()
     cy.location('pathname').should('eq','/')
