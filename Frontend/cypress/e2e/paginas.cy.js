c25bfa266ae59eafc5d28c3f1f25161dec4f6f4c
Pedro <phpontesmartins19@gmail.com>
2024-08-23 09:49:15 -0300

    :sparkles: feat: Adição dos testes de navegação

--- Frontend/cypress/e2e/paginas.cy.js
+++ Frontend/cypress/e2e/paginas.cy.js
@@ -1,16 +1,40 @@
-
 describe('Testando múltiplas páginas', () => {
+  it('Deve conseguir acessar a página de início', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('nova-transacao').should('exist').and('have.text', 'Nova Transação')
+    })
+
   it('Deve conseguir acessar a página de cartões', () => {
-  cy.visit('/')
+    cy.visit('http://localhost:3000')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('app-home').find('a').eq(1).click()
+    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
   })
-})
 
-describe('Testando múltiplas páginas', () => {
-  it('Deve conseguir acessar a página de cartões', () => {
-  cy.visit('http://localhost:3000')
-  cy.getByData('botao-login').click()
-  cy.getByData('email-input').type('neilton@alura.com')
-  cy.getByData('senha-input').type('123456')
-  cy.getByData('botao-enviar').click()
+  it('Deve conseguir acessar a página de serviços', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('app-home').find('a').eq(2).click()
+    cy.getByData('servicos').contains('Pix')
+  })
+
+  it('Deve conseguir acessar a página de investimentos', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('app-home').find('a').eq(3).click()
+    cy.getByData('investimentos').contains('Investimentos')
   })
 })
--- Frontend/src/componentes/Servicos/Icone/index.jsx
+++ Frontend/src/componentes/Servicos/Icone/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Icone({ icone, estilos }) {
   return (
-    <div className={estilos.servicos}>
+    <div className={estilos.servicos} data-test='servicos'>
       <img src={icone.imagem} alt={icone.servico} />
       <h5>{icone.servico}</h5>
     </div>
